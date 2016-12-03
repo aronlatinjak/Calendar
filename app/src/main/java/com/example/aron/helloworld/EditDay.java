6c3b9d4f36934fc88149fb6f62bf4b95a9b28abd
aronlatinjak <latinjakaron@gmail.com>
2016-12-03 16:33:55 -0500

    for david

---
+++ app/src/main/java/com/example/aron/helloworld/EditDay.java
@@ -0,0 +1,13 @@
+package com.example.aron.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditDay extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_days);
+    }
+}
+
