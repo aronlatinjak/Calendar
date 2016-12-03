87a40c6a9162ce605cb572d5033e5e82d2d671bc
aronlatinjak <latinjakaron@gmail.com>
2016-12-03 18:40:57 -0500

    for david

--- app/src/main/java/com/example/aron/helloworld/CalendarActivity.java
+++ app/src/main/java/com/example/aron/helloworld/CalendarActivity.java
@@ -2,12 +2,44 @@ package com.example.aron.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.Button;
 public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        Button day1 = (Button) findViewById(R.id.day1);
+        Button day2 = (Button) findViewById(R.id.day2);
+        Button day3 = (Button) findViewById(R.id.day3);
+        Button day4 = (Button) findViewById(R.id.day4);
+        Button day5 = (Button) findViewById(R.id.day5);
+        Button day6 = (Button) findViewById(R.id.day6);
+        Button day7 = (Button) findViewById(R.id.day7);
+        Button day8 = (Button) findViewById(R.id.day8);
+        Button day9 = (Button) findViewById(R.id.day9);
+        Button day10 = (Button) findViewById(R.id.day10);
+        Button day11 = (Button) findViewById(R.id.day11);
+        Button day12 = (Button) findViewById(R.id.day12);
+        Button day13 = (Button) findViewById(R.id.day13);
+        Button day14 = (Button) findViewById(R.id.day14);
+        Button day15 = (Button) findViewById(R.id.day15);
+        Button day16 = (Button) findViewById(R.id.day16);
+        Button day17 = (Button) findViewById(R.id.day17);
+        Button day18 = (Button) findViewById(R.id.day18);
+        Button day19 = (Button) findViewById(R.id.day19);
+        Button day20 = (Button) findViewById(R.id.day20);
+        Button day21 = (Button) findViewById(R.id.day21);
+        Button day22 = (Button) findViewById(R.id.day22);
+        Button day23 = (Button) findViewById(R.id.day23);
+        Button day24 = (Button) findViewById(R.id.day24);
+        Button day25 = (Button) findViewById(R.id.day25);
+        Button day26 = (Button) findViewById(R.id.day26);
+        Button day27 = (Button) findViewById(R.id.day27);
+        Button day28 = (Button) findViewById(R.id.day28);
+        Button day29 = (Button) findViewById(R.id.day29);
+        Button day30 = (Button) findViewById(R.id.day30);
+        Button day31 = (Button) findViewById(R.id.day31);
     }
 }
--- app/src/main/java/com/example/aron/helloworld/EditDay.java
+++ app/src/main/java/com/example/aron/helloworld/EditDay.java
@@ -2,12 +2,36 @@ package com.example.aron.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditDay extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_days);
+
+        Button cancel = (Button)findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+            }
+        });
+        Button delete= (Button)findViewById(R.id.delete);
+        delete.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+            }
+
+
+        });
+        Button ok = (Button)findViewById(R.id.ok);
+        ok.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+            }
+
+
+        });
     }
 }
-
